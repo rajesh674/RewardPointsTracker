3192c7cac4f04ea966a96011babd40eaf9712b5d
rajesh674 <mishra.rajesh674@gmail.com>
2024-07-12 15:46:35 +0530

    added comments

--- src/Hooks/useApi.js
+++ src/Hooks/useApi.js
@@ -16,6 +16,7 @@ const useApi = (url) => {
     const [loading, setLoading] = useState(false); // State to track loading state
     const [error, setError] = useState(null); // State to hold error information
 
+
     useEffect(() => {
         // useEffect hook to fetch data when component mounts or url changes
         const fetchData = async () => {
